47e169e90135e842a0d2f7fbc2ae73d22cd8baef
Cem Yilmaz <yilmaz.cem.2603@gmail.com>
2021-02-12 13:43:02 +0100

    iOS 13.0 + Compatibility

--- Sources/HorizontalItemSlider/HorizontalItemSlider.swift
+++ Sources/HorizontalItemSlider/HorizontalItemSlider.swift
@@ -1,6 +1,6 @@
 import SwiftUI
 
-@available(iOS 14.0, *)
+@available(iOS 13.0, *)
 public struct HorizontalItemSlider<Element, Content: View>: View {
 
     @State private var selection: Int = 0
@@ -13,13 +13,15 @@ public struct HorizontalItemSlider<Element, Content: View>: View {
     public var array: [Element]
     public var content: (_ element: Element) -> Content
 
+    @GestureState private var translation: CGFloat = 0
+    
     public init(
         showIndices: Bool = true,
         verticalIndicesAlignment: VerticalAlignment = VerticalAlignment.bottom,
         horizontalIndicesAlignment: HorizontalAlignment = HorizontalAlignment.center,
         array: [Element],
         @ViewBuilder content: @escaping (_ element: Element) -> Content
-        
+
     ) {
         self.showIndices = true
         self.verticalIndicesAlignment = verticalIndicesAlignment
@@ -32,19 +34,34 @@ public struct HorizontalItemSlider<Element, Content: View>: View {
 
         ZStack {
             
-            TabView(selection: self.$selection) {
+            GeometryReader { geometry in
                 
-                ForEach(0..<self.array.count) { i in
-                    
-                    self.content(self.array[i])
-                        .tag(i)
-                    
+                HStack(spacing: 0) {
+                    ForEach(0..<self.array.count) { i in
+                        
+                        
+                        self.content(self.array[i])
+                            .frame(width: geometry.size.width)
+                        
+                        
+                    }
                 }
+                .frame(width: geometry.size.width, alignment: .leading)
+                .offset(x: -CGFloat(self.selection) * geometry.size.width)
+                .offset(x: self.translation)
+                .animation(.interactiveSpring())
+                .gesture(
+                    DragGesture().updating(self.$translation) { value, state, _ in
+                        state = value.translation.width
+                    }.onEnded { value in
+                        let offset = value.translation.width / geometry.size.width
+                        let newIndex = (CGFloat(self.selection) - offset).rounded()
+                        self.selection = min(max(Int(newIndex), 0), self.array.count - 1)
+                    }
+                )
                 
-            }.tabViewStyle(
-                PageTabViewStyle(indexDisplayMode: PageTabViewStyle.IndexDisplayMode.never)
-            )
-
+            }
+            
             if self.array.count > 1 && self.showIndices {
 
                 VStack {
@@ -87,7 +104,7 @@ public struct HorizontalItemSlider<Element, Content: View>: View {
                 }.padding(8)
 
             }
-
+            
         }
 
     }
@@ -96,12 +113,12 @@ public struct HorizontalItemSlider<Element, Content: View>: View {
 
 
 
-@available(iOS 14.0, *)
+@available(iOS 13.0, *)
 public struct HorizontalItemSlider_Previews: PreviewProvider {
 
     public static var previews: some View {
 
-        NavigationView {
+        VStack {
 
             HorizontalItemSlider(
                 horizontalIndicesAlignment: HorizontalAlignment.trailing,
@@ -109,12 +126,14 @@ public struct HorizontalItemSlider_Previews: PreviewProvider {
                     "Hallo", "Welt", "!"
                 ]
             ) { element in
-                
+
                 Text("\(element)")
                     .frame(minWidth: 0, idealWidth: 100, maxWidth: .infinity, minHeight: 0, idealHeight: 100, maxHeight: .infinity, alignment: .center)
                     .background(Color.red)
-                
+
             }.frame(height: 256)
+            
+            Spacer()
 
         }.colorScheme(.dark)
 
